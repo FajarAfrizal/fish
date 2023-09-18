e001dbcec5c0f49432e9ae1963a2012941b33fc4
Fajar Afrizal <fajarmuhammadafrizal@smkwikrama.sch.id>
2023-09-18 14:13:12 +0700

    cors add

--- app.js
+++ app.js
@@ -21,15 +21,15 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "*"
-  ); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+app.options("/*", function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.sendStatus(200);
+});
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
